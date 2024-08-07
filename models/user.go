4e768913f4fe932fd0f1f32a9d608f1ca81f5d8b
ByteNinja <pijayblog@gmail.com>
2024-08-07 12:10:11 +0700

    Update user.go

--- models/user.go
+++ models/user.go
@@ -16,26 +16,3 @@ type User struct {
     CreatedAt         time.Time `json:"created_at"`
     UpdatedAt         time.Time `json:"updated_at"`
 }
-
-type Role struct {
-    RoleID    uint      `gorm:"primary_key" json:"role_id"`
-    RoleName  string    `gorm:"type:varchar(100);unique_index" json:"role_name"`
-    CreatedAt time.Time `json:"created_at"`
-    UpdatedAt time.Time `json:"updated_at"`
-}
-
-type Group struct {
-    ID        uint      `gorm:"primary_key" json:"id"`
-    RoleID    uint      `json:"role_id"`
-    UserID    uint      `json:"user_id"`
-    CreatedAt time.Time `json:"created_at"`
-    UpdatedAt time.Time `json:"updated_at"`
-    Role      Role      `gorm:"foreignkey:RoleID"`
-    User      User      `gorm:"foreignkey:UserID"`
-}
-
-func Migrate(db *gorm.DB) {
-    db.AutoMigrate(&User{})
-    db.AutoMigrate(&Role{})
-    db.AutoMigrate(&Group{})
-}
